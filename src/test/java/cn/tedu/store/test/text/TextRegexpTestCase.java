c4038a0ec4bfd176d82a15c13b430574e942dd77
Administrator <Administrator@8F64FBY3KVR2HEX>
2019-10-21 17:31:23 +0800

    0

--- src/test/java/cn/tedu/store/test/text/TextRegexpTestCase.java
+++ src/test/java/cn/tedu/store/test/text/TextRegexpTestCase.java
@@ -6,10 +6,10 @@ public class TextRegexpTestCase {
 	
 	public static void main(String[] args) {
 		 // 55-88  
-        String regex = "^ABC([5-7]+[0-9]+|[8]+[8]+)$";  
+		String reg = "[2,4,6,8]|[1-9][0,2,4,6,8]|100";
        // String regex1 = "^ABC(00[1-9]|0[1-9][0-9]|100)$";  
-        String str="ABC59";
-      System.out.println(str.matches(regex));
+        String str="50";
+      System.out.println(str.matches(reg));
   
 	}
 
