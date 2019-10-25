b20bf659ab1cb878502093e20203699d1bc878d2
spq580231 <spq580231@6163.com>
2019-10-25 09:58:43 +0800

    tesT

--- src/main/java/cn/tedu/store/entity/Address.java
+++ src/main/java/cn/tedu/store/entity/Address.java
@@ -2,132 +2,132 @@ package cn.tedu.store.entity;
 
 public class Address extends BaseEntity {
 
-	private static final long serialVersionUID = 3648890010320219195L;
-	
-	private Integer id;
-	private Integer uid;
-	private String recvName;
-	private String recvProvince;
-	private String recvCity;
-	private String recvArea;
-	private String recvDistrict;
-	private String recvAddress;
-	private String recvPhone;
-	private String recvTel;
-	private String recvZip;
-	private String recvTag;
-	private Integer isDefault;
+    private static final long serialVersionUID = 3648890010320219195L;
 
-	public Integer getId() {
-		return id;
-	}
+    private Integer id;
+    private Integer uid;
+    private String recvName;
+    private String recvProvince;
+    private String recvCity;
+    private String recvArea;
+    private String recvDistrict;
+    private String recvAddress;
+    private String recvPhone;
+    private String recvTel;
+    private String recvZip;
+    private String recvTag;
+    private Integer isDefault;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public Integer getUid() {
-		return uid;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setUid(Integer uid) {
-		this.uid = uid;
-	}
+    public Integer getUid() {
+        return uid;
+    }
 
-	public String getRecvName() {
-		return recvName;
-	}
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
 
-	public void setRecvName(String recvName) {
-		this.recvName = recvName;
-	}
+    public String getRecvName() {
+        return recvName;
+    }
 
-	public String getRecvProvince() {
-		return recvProvince;
-	}
+    public void setRecvName(String recvName) {
+        this.recvName = recvName;
+    }
 
-	public void setRecvProvince(String recvProvince) {
-		this.recvProvince = recvProvince;
-	}
+    public String getRecvProvince() {
+        return recvProvince;
+    }
 
-	public String getRecvCity() {
-		return recvCity;
-	}
+    public void setRecvProvince(String recvProvince) {
+        this.recvProvince = recvProvince;
+    }
 
-	public void setRecvCity(String recvCity) {
-		this.recvCity = recvCity;
-	}
+    public String getRecvCity() {
+        return recvCity;
+    }
 
-	public String getRecvArea() {
-		return recvArea;
-	}
+    public void setRecvCity(String recvCity) {
+        this.recvCity = recvCity;
+    }
 
-	public void setRecvArea(String recvArea) {
-		this.recvArea = recvArea;
-	}
+    public String getRecvArea() {
+        return recvArea;
+    }
 
-	public String getRecvDistrict() {
-		return recvDistrict;
-	}
+    public void setRecvArea(String recvArea) {
+        this.recvArea = recvArea;
+    }
 
-	public void setRecvDistrict(String recvDistrict) {
-		this.recvDistrict = recvDistrict;
-	}
+    public String getRecvDistrict() {
+        return recvDistrict;
+    }
 
-	public String getRecvAddress() {
-		return recvAddress;
-	}
+    public void setRecvDistrict(String recvDistrict) {
+        this.recvDistrict = recvDistrict;
+    }
 
-	public void setRecvAddress(String recvAddress) {
-		this.recvAddress = recvAddress;
-	}
+    public String getRecvAddress() {
+        return recvAddress;
+    }
 
-	public String getRecvPhone() {
-		return recvPhone;
-	}
+    public void setRecvAddress(String recvAddress) {
+        this.recvAddress = recvAddress;
+    }
 
-	public void setRecvPhone(String recvPhone) {
-		this.recvPhone = recvPhone;
-	}
+    public String getRecvPhone() {
+        return recvPhone;
+    }
 
-	public String getRecvTel() {
-		return recvTel;
-	}
+    public void setRecvPhone(String recvPhone) {
+        this.recvPhone = recvPhone;
+    }
 
-	public void setRecvTel(String recvTel) {
-		this.recvTel = recvTel;
-	}
+    public String getRecvTel() {
+        return recvTel;
+    }
 
-	public String getRecvZip() {
-		return recvZip;
-	}
+    public void setRecvTel(String recvTel) {
+        this.recvTel = recvTel;
+    }
 
-	public void setRecvZip(String recvZip) {
-		this.recvZip = recvZip;
-	}
+    public String getRecvZip() {
+        return recvZip;
+    }
 
-	public String getRecvTag() {
-		return recvTag;
-	}
+    public void setRecvZip(String recvZip) {
+        this.recvZip = recvZip;
+    }
 
-	public void setRecvTag(String recvTag) {
-		this.recvTag = recvTag;
-	}
+    public String getRecvTag() {
+        return recvTag;
+    }
 
-	public Integer getIsDefault() {
-		return isDefault;
-	}
+    public void setRecvTag(String recvTag) {
+        this.recvTag = recvTag;
+    }
 
-	public void setIsDefault(Integer isDefault) {
-		this.isDefault = isDefault;
-	}
+    public Integer getIsDefault() {
+        return isDefault;
+    }
 
-	@Override
-	public String toString() {
-		return "Address [id=" + id + ", uid=" + uid + ", recvName=" + recvName + ", recvProvince=" + recvProvince
-				+ ", recvCity=" + recvCity + ", recvArea=" + recvArea + ", recvDistrict=" + recvDistrict
-				+ ", recvAddress=" + recvAddress + ", recvPhone=" + recvPhone + ", recvTel=" + recvTel + ", recvZip="
-				+ recvZip + ", recvTag=" + recvTag + ", isDefault=" + isDefault + "]";
-	}
+    public void setIsDefault(Integer isDefault) {
+        this.isDefault = isDefault;
+    }
+
+    @Override
+    public String toString() {
+        return "Address [id=" + id + ", uid=" + uid + ", recvName=" + recvName + ", recvProvince=" + recvProvince
+                + ", recvCity=" + recvCity + ", recvArea=" + recvArea + ", recvDistrict=" + recvDistrict
+                + ", recvAddress=" + recvAddress + ", recvPhone=" + recvPhone + ", recvTel=" + recvTel + ", recvZip="
+                + recvZip + ", recvTag=" + recvTag + ", isDefault=" + isDefault + "]";
+    }
 
 }
--- src/main/java/cn/tedu/store/interceptor/LoginInterceptor.java
+++ src/main/java/cn/tedu/store/interceptor/LoginInterceptor.java
@@ -11,39 +11,39 @@ import org.springframework.web.servlet.ModelAndView;
  * 登录拦截器，如果用户没有登录就尝试访问某些URL，将被重定向到登录页
  */
 public class LoginInterceptor
-	implements HandlerInterceptor {
+        implements HandlerInterceptor {
 
-	public boolean preHandle(
-			HttpServletRequest request, 
-			HttpServletResponse response, 
-			Object handler)
-			throws Exception {
-		// 从request中获取session
-		HttpSession session
-			= request.getSession();
-		// 判断session中是否存在uid
-		if (session.getAttribute("uid") != null) {
-			// 存在：用户已登录，则放行
-			return true;
-		} else {
-			// 不存在：用户未登录，重定向，且拦截
-			response.sendRedirect("../web/login.html");
-			return false;
-		}
-	}
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler)
+            throws Exception {
+        // 从request中获取session
+        HttpSession session
+                = request.getSession();
+        // 判断session中是否存在uid
+        if (session.getAttribute("uid") != null) {
+            // 存在：用户已登录，则放行
+            return true;
+        } else {
+            // 不存在：用户未登录，重定向，且拦截
+            response.sendRedirect("../web/login.html");
+            return false;
+        }
+    }
 
-	public void postHandle(
-			HttpServletRequest request, 
-			HttpServletResponse response, 
-			Object handler,
-			ModelAndView modelAndView) throws Exception {
-	}
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            ModelAndView modelAndView) throws Exception {
+    }
 
-	public void afterCompletion(
-			HttpServletRequest request, 
-			HttpServletResponse response, 
-			Object handler, Exception ex)
-			throws Exception {
-	}
+    public void afterCompletion(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler, Exception ex)
+            throws Exception {
+    }
 
 }
